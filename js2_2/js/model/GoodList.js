2c47cac707cb0b41b45115f6d081d3b400d5026e
Anastasia <koxinataaoi@yandex.com>
2021-07-08 16:35:29 +0300

    первые 2 задания дз2

---
+++ js2_2/js/model/GoodList.js
@@ -0,0 +1,27 @@
+import Good from "./Good.js";
+
+export default class GoodList {
+    constructor(goods) {
+        this.goods = goods.map(item => new Good(item));
+    }
+
+    get() {
+        return this.goods;
+    }
+
+    add(good) {
+        this.goods.push(good);
+    }
+
+    remove(id) {
+        const index = this.goods.findIndex(good => good.id === id);
+        this.goods.splice(index, 1);
+    }
+
+    cost(totalCost) {
+        totalCost = this.goods.forEach(function(){
+            this.goods.getPrice() + totalCost;
+        });
+        return totalCost;
+    }
+}
